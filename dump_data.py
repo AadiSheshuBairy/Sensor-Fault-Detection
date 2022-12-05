4c9f7afc2bc7f166e06c669791b87f1f2a09e773
AadiSheshuBairy <aadigoudbairy959@gmail.com>
2022-12-05 12:38:44 +0530

    updated1

--- dump_data.py
+++ dump_data.py
@@ -1,7 +1,6 @@
 import pymongo
 import pandas as pd
 import json
-
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
@@ -12,17 +11,14 @@ COLLECTION_NAME="sensor"
 
 if __name__=="__main__":
     df = pd.read_csv(DATA_FILE_PATH)
-    print(f"Rows and columns:{df.shape}")
+    print(f"Rows and columns: {df.shape}")
 
-    #convert dataframe to json so that we can dump the data into MongoDb
+    #Convert dataframe to json so that we can dump these record in mongo db
     df.reset_index(drop=True,inplace=True)
 
-    json_record=list(json.loads(df.T.to_json()).values())
+    json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
 
-    #insert converted json record to Mongo Db
+    #insert converted json record to mongo db
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
 
-
-
-    
